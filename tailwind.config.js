0239004b2d4f310cafb19687b20213010e2d5c09
Andrés V <andres@valsis.cl>
2022-05-06 19:36:21 -0400

    Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     content: ['./src/**/*.js'],
     darkMode: 'media',
     theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+		},
+		fontSize: {
+			'4xl': '2.75rem'
+		},
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
