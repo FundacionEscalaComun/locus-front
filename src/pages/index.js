1c7a83ea6dcd759fbcf9adbf602fbbe8b29b1c1f
Andrés V <andres@valsis.cl>
2022-05-27 19:03:20 -0400

    Ajustes móvil.

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,7 +62,7 @@ export default function Home() {
 						<Swiper
 						modules={[Navigation]}
 						spaceBetween={50}
-						slidesPerView={(window.innerWidth <= 768)? 1 : 3}
+						slidesPerView={(typeof window !== "undefined" && window.innerWidth <= 768)? 1 : 3}
 						navigation={{ 
 							clickable: true, 
 						}}
@@ -101,61 +101,63 @@ export default function Home() {
 					<div className='container pt-40'>
 						<h2 className='font-hbold text-3xl lg:text-4xl mb-12 text-center'>EQUIPO</h2>
 						<div className='flex flex-wrap justify-center gap-y-20 text-center pb-10'>
-							<article className='w-1/2 lg:w-1/3'>
+							<article className='w-full lg:w-1/3'>
 								<div className='relative h-48'><Image src="/img/fotos/fotos-piera-medina.svg" layout='fill' objectFit='contain' alt='foto' /></div>
 								<h3 className='font-bold mt-6 mb-2 text-xl'>Piera Medina</h3>
 								<p className='text-sm font-medium'>DIRECTORA GENERAL</p>
 							</article>
-							<article className='w-1/2 lg:w-1/3'>
+							<article className='w-full lg:w-1/3'>
 								<div className='relative h-48'><Image src="/img/fotos/fotos-danitza-vymazal.svg" layout='fill' objectFit='contain' alt='foto' /></div>
 								<h3 className='font-bold mt-6 mb-2 text-xl'>Danitza Vymazal</h3>
 								<p className='text-sm font-medium'>DIRECTORA DE PRODUCCIÓN</p>
 							</article>
-							<article className='w-1/2 lg:w-1/3'>
+							<article className='w-full lg:w-1/3'>
 								<div className='relative h-48'><Image src="/img/fotos/fotos-carole-gurdon.svg" layout='fill' objectFit='contain' alt='foto' /></div>
 								<h3 className='font-bold mt-6 mb-2 text-xl'>Carole Gurdon</h3>
 								<p className='text-sm font-medium'>DIRECTORA DE CONTENIDOS</p>
 							</article>
-							<article className='w-1/2 lg:w-1/3'>
+							<article className='w-full lg:w-1/3'>
 								<div className='relative h-48'><Image src="/img/fotos/fotos-macarena-cima.svg" layout='fill' objectFit='contain' alt='foto' /></div>
 								<h3 className='font-bold mt-6 mb-2 text-xl'>Macarena Cima</h3>
 								<p className='text-sm font-medium'>ASISTENTE DE PRODUCCIÓN</p>
 							</article>
-							<article className='w-1/2 lg:w-1/3'>
+							<article className='w-full lg:w-1/3'>
 								<div className='relative h-48'><Image src="/img/fotos/fotos-fernanda-chesta.svg" layout='fill' objectFit='contain' alt='foto' /></div>
 								<h3 className='font-bold mt-6 mb-2 text-xl'>Fernanda Chesta</h3>
 								<p className='text-sm font-medium'>ASISTENTE DE CONTENIDOS</p>
 							</article>
 						</div>
-
-						<div className='grid lg:grid-cols-3 gap-8 my-10 text-center'>
-							<article>
-								<h3 className='font-hbold mb-4 text-xl'>PRENSA</h3>
-								<p className='font-semibold'>Agencia Minka</p>
-							</article>
-							<article>
-								<h3 className='font-hbold mb-4 text-xl'>COMUNICACIONES</h3>
-								<p className='font-semibold'>Macarena Ovalle</p>
-							</article>
-							<article>
-								<h3 className='font-hbold mb-4 text-xl'>REDES SOCIALES</h3>
-								<p className='font-semibold'>Teresita Quezada</p>
-							</article>
 						</div>
+						<div className='fondo-rosado'>
+							<div className='container'>
+								<div className='grid lg:grid-cols-3 gap-8 my-10 text-center'>
+									<article>
+										<h3 className='font-hbold mb-4 text-xl'>PRENSA</h3>
+										<p className='font-semibold'>Agencia Minka</p>
+									</article>
+									<article>
+										<h3 className='font-hbold mb-4 text-xl'>COMUNICACIONES</h3>
+										<p className='font-semibold'>Macarena Ovalle</p>
+									</article>
+									<article>
+										<h3 className='font-hbold mb-4 text-xl'>REDES SOCIALES</h3>
+										<p className='font-semibold'>Teresita Quezada</p>
+									</article>
+								</div>
 
-						<h2 className='font-hbold text-xl mt-20 mb-4 text-center'>DISEÑO</h2>
-						<div className='grid lg:grid-cols-3 gap-8 text-center font-semibold'>
-							<p>Sergio Ramírez</p>
-							<p>Valentina Alcántara</p>
-							<p>Martín Pastenes</p>
-						</div>
+								<h2 className='font-hbold text-xl mt-20 mb-4 text-center'>DISEÑO</h2>
+								<div className='grid lg:grid-cols-3 gap-8 text-center font-semibold'>
+									<p>Sergio Ramírez</p>
+									<p>Valentina Alcántara</p>
+									<p>Martín Pastenes</p>
+								</div>
 
-						<h2 className='font-hbold text-xl mt-20 mb-4 text-center'>AUDIOVISUAL</h2>
-						<div className='grid gap-8 text-center'>
-							<p className='font-semibold'>Los Navegantes Films</p>
+								<h2 className='font-hbold text-xl mt-20 mb-4 text-center'>AUDIOVISUAL</h2>
+								<div className='grid gap-8 text-center'>
+									<p className='font-semibold'>Los Navegantes Films</p>
+								</div>
+							</div>
 						</div>
-						
-					</div>
 				</section>
 
 				<section className='pt-52 pb-16 lg:pb-40 fondo-rosado fondo-figura-blanca' id='redes'>
