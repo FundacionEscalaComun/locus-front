4da6336a3e96844d9e6efc7488edfc84f896d9e2
Andrés V <andres@valsis.cl>
2022-09-15 15:31:14 -0300

    Inicio: estrella marquesina.

--- src/components/MarqueeContent.js
+++ src/components/MarqueeContent.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function MarqueeContent() {
 	return (
 		<span className='block py-6 fondo-verde color-blanco'>
-			<span className='mx-4 align-middle'><Image src="/img/flor-azul.svg" width={16} height={16} alt='' /></span>
+			<span className='mx-4 align-middle'><Image src="/img/estrella-marquesina.svg" width={16} height={16} alt='' /></span>
 			<strong className='font-hlight'>¡REVISA A LOS SELECCIONADOS DEL CONCURSO GENIUS LOCI! </strong>
 		</span>
 	)
