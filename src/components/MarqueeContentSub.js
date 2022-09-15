97fe078020c9c2ecb85c6f0b157dc5156a9dc102
Andrés V <andres@valsis.cl>
2022-09-15 12:39:53 -0300

    Inicio: quita lugar de marquesina.

--- src/components/MarqueeContentSub.js
+++ src/components/MarqueeContentSub.js
@@ -7,8 +7,6 @@ export default function MarqueeContentSub() {
 			<span className='mx-4 align-text-top'><Image src="/img/flor-rosada.svg" width={20} height={20} alt='' /></span>
 			<strong className='font-hbold text-sm'>SALIDA ESTACIÓN METRO VIÑA</strong>
 			<span className='mx-4 align-text-top'><Image src="/img/flor-rosada.svg" width={20} height={20} alt='' /></span>
-			<strong className='font-hbold text-sm'>PLAZA POBLACIÓN RIESCO</strong>
-			<span className='mx-4 align-text-top'><Image src="/img/flor-rosada.svg" width={20} height={20} alt='' /></span>
 			<strong className='font-hbold text-sm'>SKATEPARK SAUSALITO</strong>
 			<span className='mx-4 align-text-top'><Image src="/img/flor-rosada.svg" width={20} height={20} alt='' /></span>
 			<strong className='font-hbold text-sm'>PLAZA PALACIO CARRASCO </strong>
