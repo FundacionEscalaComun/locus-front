887a5b2c586e59c4e297d8274d2d3ef8dd694314
Andrés V <andres@valsis.cl>
2022-09-26 19:52:11 -0300

    Video: dimensiones iframe y fondo.

--- src/pages/video/[id].js
+++ src/pages/video/[id].js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
-export default function Video() {
+export default function Video({youtubeId}) {
     return (
       <>
         <Head>
@@ -12,13 +12,37 @@ export default function Video() {
         </Head>
 
         <Header />
-			  <main>
-          <section>
-            <div className='container pt-20'>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/iFJbOunLwWY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+			  <main className='video flex items-center justify-center'>
+          <section className='w-full'>
+            <div className='container'>
+              <iframe width="560" height="315" src={`https://www.youtube.com/embed/${youtubeId}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
           </section>
 			  </main>
       </>
     )
+}
+
+export async function getStaticPaths() {
+	const res = await fetch(process.env.NEXT_PUBLIC_WP_URL + '/video?_embed&per_page=96&order=asc&t=' + Math.round(new Date().getTime()/1000))
+  let videos = await res.json()
+  const paths = videos.map(video => ({
+    params:{id:video.id.toString()}
+  }))
+  return {
+    paths,
+    fallback: false, // can also be true or 'blocking'
+  }
+}
+
+export async function getStaticProps(context) {
+	const res = await fetch(process.env.NEXT_PUBLIC_WP_URL + '/video/' + context.params.id + '?t=' + Math.round(new Date().getTime()/1000))
+  let video = await res.json()
+  let youtubeId = video.acf.id_youtube.replace('https://youtu.be/', '')
+
+  return {
+    props: {
+			youtubeId,
+		},
+  }
 }
