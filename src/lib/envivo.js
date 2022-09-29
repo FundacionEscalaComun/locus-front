6bb146773d42ba7ed5fe5ad5a38bc42ef7a92481
Andrés V <andres@valsis.cl>
2022-09-29 19:58:16 -0300

    En vivo: prueba.

--- src/lib/envivo.js
+++ src/lib/envivo.js
@@ -1,6 +1,6 @@
 export function getEnVivo() {
   const ahora = new Date();
-  const ensayo = ahora >= new Date('2022-09-29 19:32:00 GMT-03:00') && ahora <= new Date('2022-09-29 19:48:00 GMT-03:00');
+  const ensayo = ahora >= new Date('2022-09-29 20:01:00 GMT-03:00') && ahora <= new Date('2022-09-29 20:03:00 GMT-03:00');
   const martes = ahora >= new Date('2022-10-04 18:30:00 GMT-03:00') && ahora <= new Date('2022-10-04 19:30:00 GMT-03:00');
   const miercoles = ahora >= new Date('2022-10-05 18:30:00 GMT-03:00') && ahora <= new Date('2022-10-05 19:30:00 GMT-03:00');
   const jueves = ahora >= new Date('2022-10-06 18:30:00 GMT-03:00') && ahora <= new Date('2022-10-06 19:30:00 GMT-03:00');
