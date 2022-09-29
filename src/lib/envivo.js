27880f48d147d8c8ebd030ec64f1bbd4a322eb56
Andrés V <andres@valsis.cl>
2022-09-29 18:21:39 -0300

    En vivo: prueba cabecera.

--- src/lib/envivo.js
+++ src/lib/envivo.js
@@ -1,5 +1,5 @@
 const ahora = new Date();
-const ensayo = ahora >= new Date('2022-09-29 17:00:00 GMT-03:00') && ahora <= new Date('2022-09-29 18:00:00 GMT-03:00');
+const ensayo = ahora >= new Date('2022-09-29 17:00:00 GMT-03:00') && ahora <= new Date('2022-09-29 19:00:00 GMT-03:00');
 const martes = ahora >= new Date('2022-10-04 18:30:00 GMT-03:00') && ahora <= new Date('2022-10-04 19:30:00 GMT-03:00');
 const miercoles = ahora >= new Date('2022-10-05 18:30:00 GMT-03:00') && ahora <= new Date('2022-10-05 19:30:00 GMT-03:00');
 const jueves = ahora >= new Date('2022-10-06 18:30:00 GMT-03:00') && ahora <= new Date('2022-10-06 19:30:00 GMT-03:00');
