81668089cd81cfbfb7253400690b972299b12a52
Andrés V <andres@valsis.cl>
2022-09-29 19:27:52 -0300

    En vivo: prueba className completo condicional.

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
 	}
 
 	return (
-		<header className={'header fondo-azul lg:flex ' + (enVivo? 'header-envivo' : '')}>
+		<header className={enVivo? 'header header-envivo fondo-azul lg:flex' : 'header fondo-azul lg:flex'}>
 			<div className='container flex justify-between items-center lg:grid grid-cols-4 lg:items-stretch gap-8'>
 				<h1 className='w-16 h-16 mt-3 relative hidden lg:block'><Link href='/'><a><Image src={enVivo? "/img/logos/en-vivo-22.svg" : "/img/logos/web-locus-logo-header.svg"} layout='fill' objectFit='contain' alt='LOCUS' /></a></Link></h1>
 				<h1 className='w-16 h-16 mt-3 relative lg:hidden'><Link href='/'><a><Image src="/img/logos/web-locus-logo-header.svg" layout='fill' objectFit='contain' alt='LOCUS' /></a></Link></h1>
